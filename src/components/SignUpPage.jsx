94dfea0e53aee00deca3ce37f0fdae0cdb38ee43
Srinithi1916 <srinithisrinithi09@gmail.com>
2023-11-21 10:24:06 +0530

    commit2

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,48 +1,86 @@
-import React from 'react';
-import { Paper, Stack, TextField, Typography, Button, Link } from '@mui/material';
-import background from './img/background-imag.jpg';
-import TextFieldbox from './TextFieldbox';
-import CheckboxElement from './CheckboxElement';
-import Navbar2 from './Navbar2'
+import React from "react";
+import {
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+  Button,
+  Link,
+} from "@mui/material";
+import background from "./img/background-imag.jpg";
+import TextFieldbox from "./TextFieldbox";
+import CheckboxElement from "./CheckboxElement";
+import Navbar2 from "./Navbar2";
 
 const SignUpPage = () => {
-
-    const size = {width : '500px' , height: '700px',  marginTop: '70px'};
-    const setCenter = {display : 'flex', justifyContent : 'center', alignItems : 'center',};
+  const size = { width: "500px", height: "700px", marginTop: "70px" };
+  const setCenter = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
   return (
     <>
-    <Navbar2/>
-    <div style = {{backgroundImage: `url(${background})`, width: '100vw' , height: '100vh', display : 'flex',justifyContent : 'center' , alignItems : 'center'}}>  
-    <Paper elevation={20} square={false} style = {size}>
-      
-      <Stack spacing = {1} direction = {'column'} align = {'center'} style = {{setCenter, padding : '50px'}}>
+      <Navbar2 />
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Paper elevation={20} square={false} style={size}>
+          <Stack
+            spacing={1}
+            direction={"column"}
+            align={"center"}
+            style={{ setCenter, padding: "50px" }}
+          >
+            <Typography variant="h3" fontFamily={'"SemiBold",Poppins'}>
+              SignUp
+            </Typography>
+            <br></br>
+            <TextFieldbox
+              ElementName={"Username"}
+              id={"username"}
+              placeHolderText={"Username or Email"}
+            ></TextFieldbox>
+            <br></br>
+            <TextFieldbox
+              ElementName={"Create Password"}
+              id={"password"}
+              placeHolderText={"CreatePassword"}
+            ></TextFieldbox>
 
-        <Typography variant = 'h3' fontFamily={'"SemiBold",Poppins'}>SignUp</Typography><br></br>
-        <TextFieldbox ElementName={'Username'} id={'username'} placeHolderText={'Username or Email'}></TextFieldbox>
-        <br></br>
-        <TextFieldbox ElementName={'Create Password'} id={'password'} placeHolderText={'CreatePassword'}></TextFieldbox>
-       
-        <br></br>
-        <TextFieldbox ElementName={'Confirm Password'} id={'password'} placeHolderText={'ConfirmPassword'}></TextFieldbox>
-        <br></br>
-        <CheckboxElement CheckboxLabel={'I have accepted the '} linkname = {'terms and conditions'}></CheckboxElement>
+            <br></br>
+            <TextFieldbox
+              ElementName={"Confirm Password"}
+              id={"password"}
+              placeHolderText={"ConfirmPassword"}
+            ></TextFieldbox>
+            <br></br>
+            <CheckboxElement
+              CheckboxLabel={"I have accepted the "}
+              linkname={"terms and conditions"}
+            ></CheckboxElement>
 
-        <br></br>
-        <br></br>
-        <Button style = {{borderRadius: '40px', backgroundColor: 'green'}} variant='contained' size = 'large' >SIGN UP</Button>
-        <br></br>
-        
-
-      </Stack>
-        
-
-    </Paper>
-          
-                
-        
-    </div>
+            <br></br>
+            <br></br>
+            <Button
+              style={{ borderRadius: "40px", backgroundColor: "green" }}
+              variant="contained"
+              size="large"
+            >
+              SIGN UP
+            </Button>
+            <br></br>
+          </Stack>
+        </Paper>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 export default SignUpPage;
